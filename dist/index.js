1c285c748e0700f60c4703d2f04cc7e997fd9090
Jiocoders <Jiocoders@gmail.com>
2025-09-07 23:22:16 +0530

    add/css-image

--- dist/index.js
+++ dist/index.js
@@ -4,16 +4,21 @@ var express = require("express");
 var path = require("path");
 var app = express();
 var port = parseInt(process.env.PORT) || process.argv[3] || 8080;
-app.use(express.static(path.join(__dirname, 'public')))
-    .set('views', path.join(__dirname, 'views'))
-    .set('view engine', 'ejs');
-app.get('/', function (req, res) {
-    res.render('index');
+// Serve static files from public folder
+// Set view engine to EJS
+app
+    .use(express.static(path.join(__dirname, "public")))
+    .set("views", path.join(__dirname, "views"))
+    .set("view engine", "ejs");
+// Routes
+app.get("/", function (req, res) {
+    res.render("index");
 });
-app.get('/api', function (req, res) {
-    res.json({ "msg": "Hello world" });
+app.get("/api", function (req, res) {
+    res.json({ msg: "Helloo world" });
 });
+// Start server
 app.listen(port, function () {
-    console.log("Listening on http://localhost:".concat(port));
+    console.log("\uD83D\uDE80 Server listening on http://localhost:".concat(port));
 });
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -4,18 +4,23 @@ import * as path from "path";
 const app = express();
 const port = parseInt(process.env.PORT) || process.argv[3] || 8080;
 
-app.use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs');
+// Serve static files from public folder
+// Set view engine to EJS
+app
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-  res.render('index');
+// Routes
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get('/api', (req, res) => {
-  res.json({"msg": "Hello world"});
+app.get("/api", (req, res) => {
+  res.json({ msg: "Helloo world" });
 });
 
+// Start server
 app.listen(port, () => {
-  console.log(`Listening on http://localhost:${port}`);
+  console.log(`🚀 Server listening on http://localhost:${port}`);
 });
