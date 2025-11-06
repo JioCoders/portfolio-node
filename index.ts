92a65c5eaa1596fe2a02f7206254ed979ddd693d
Jiocoders <Jiocoders@gmail.com>
2025-11-06 19:29:52 +0530

    update/ejs-issue

--- index.ts
+++ index.ts
@@ -9,17 +9,17 @@ const port = parseInt(process.env.PORT) || process.argv[3] || 3000;
 // Serve static files from public folder
 // Set view engine to EJS
 app
-  .use(express.static(path.join(__dirname, "public")))
-  .set("views", path.join(__dirname, "views"))
-  .set("view engine", "ejs");
+  .use(express.static(path.join(process.cwd(), 'public')))
+  .set('views', path.join(process.cwd(), 'views'))
+  .set('view engine', 'ejs');
 
 // Routes
-app.get("/", (req, res) => {
-  res.render("index");
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
-app.get("/api", (req, res) => {
-  res.json({ msg: "Helloo world" });
+app.get('/api', (req, res) => {
+  res.json({ msg: 'Helloo world' });
 });
 
 // Start server
